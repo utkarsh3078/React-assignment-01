9e12c6f827d814faf88947405f1ab3e1bd7e501e
Utkarsh Yadav <utkarshyadav3078@gmail.com>
2026-05-04 18:34:14 +0530

    Update HomePage component: refine descriptions for jokes and quotes sections

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 function HomePage() {
   return (
     <>
+      {/* Joke section */}
       <section className="card">
         <h1 className="home-title">Jokes</h1>
         <p className="home-description">
@@ -15,11 +16,12 @@ function HomePage() {
         </div>
       </section>
       <br></br>
+      {/* Quote section */}
       <section className="card">
         <h1 className="home-title">Quotes</h1>
         <p className="home-description">
-          Use the navbar buttons to move to the jokes page and fetch a random
-          joke from the API.
+          Use the navbar buttons to move to the quotes page and fetch a random
+          quote from the API.
         </p>
         <div className="button-row">
           <Link to="/quotes" className="button primary">
